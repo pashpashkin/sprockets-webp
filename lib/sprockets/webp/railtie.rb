fd24d1f6ec23854d239eaca2bdddd9b6f0c6bcc0
Pavel Kuzovkin <p.kuzovkin@corp.mail.ru>
2019-01-31 19:21:00 +0300

    Change mime types registration

--- lib/sprockets/webp/railtie.rb
+++ lib/sprockets/webp/railtie.rb
@@ -5,12 +5,12 @@ module Sprockets
     class Railtie < ::Rails::Railtie
       initializer :webp, group: :all do |app|
         app.config.assets.configure do |env|
-          env.register_mime_type 'image/jpeg', '.jpeg'
+          env.register_mime_type 'image/jpeg', extensions: ['.jpg', '.jpeg']
           env.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
             Converter.process(app, context, data)
           end
 
-          env.register_mime_type 'image/png', '.png'
+          env.register_mime_type 'image/png', extensions: ['.png']
           env.register_postprocessor 'image/png', :png_webp do |context, data|
             Converter.process(app, context, data)
           end
